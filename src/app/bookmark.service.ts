87ec52b8dc2ee5700c753b7bd0a9e8a9547fa698
alexisdelage <alexisd.reliefapps@gmail.com>
2022-04-18 11:18:50 +0200

    Create services with database

--- src/app/bookmark.service.ts
+++ src/app/bookmark.service.ts
@@ -1,9 +1,52 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs';
+
+import Video from './video';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookmarkService {
 
-  constructor() { }
+  private bookmarkUrl = 'api/bookmark';
+
+  private headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+  constructor(private http: HttpClient) { }
+
+  getBookmarkList(): Observable<Video[]> {
+    return this.http.get<Video[]>(this.bookmarkUrl).pipe(
+      tap(_ => console.log("get bookmark list")),
+      catchError((err) => {console.log(err); return of([])})
+    )
+  }
+
+  addBookmark(video: Video): void {
+    const body = {
+      url: video.getUrl()
+    }
+    const otherHttpOptions = {
+      headers: this.headers
+    }
+    this.http.post(this.bookmarkUrl, body, otherHttpOptions).pipe(
+      tap(_ => console.log("add video in bookmarks")),
+      catchError((err) => {console.log(err); return of(null)})
+    )
+  }
+
+  deleteBookmark(video: Video): void {
+    const httpOptions = {
+      headers: this.headers,
+      body: {
+        url: video.getUrl()
+      }
+    }
+    this.http.delete(this.bookmarkUrl, httpOptions).pipe(
+      tap(_ => console.log("add video in history")),
+      catchError((err) => {console.log(err); return of(null)})
+    )
+  }
+
 }
--- src/app/history.service.ts
+++ src/app/history.service.ts
@@ -1,12 +1,38 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs';
 
-import { TemplateVideo } from './interfaces';
+import Video from './video';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HistoryService {
 
-  constructor() { }
+  private historyUrl = 'api/history';
+
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  getHistoryList(): Observable<Video[]> {
+    return this.http.get<Video[]>(this.historyUrl).pipe(
+      tap(_ => console.log("get history list")),
+      catchError((err) => {console.log(err); return of([])})
+    )
+  }
+
+  addHistory(video: Video): void {
+    const body = {
+      url: video.getUrl()
+    }
+    this.http.post(this.historyUrl, body, this.httpOptions).pipe(
+      tap(_ => console.log("add video in history")),
+      catchError((err) => {console.log(err); return of(null)})
+    )
+  }
+
 }
